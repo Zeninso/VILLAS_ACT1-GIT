e534adf95456df06913e1c929d852c3cce165092
Vincent Villas <152830911+Zeninso@users.noreply.github.com>
2025-04-03 09:36:04 +0800

    Update the multiplication message

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// this will subtract the two numbers
+// this will multiply the two numbers
 static double Multiplication(double a, double b)
 {
     return a * b;
