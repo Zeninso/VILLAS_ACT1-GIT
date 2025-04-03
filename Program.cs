b60c8918333f553b5c88f69762f3f3764450d13f
Vincent Villas <152830911+Zeninso@users.noreply.github.com>
2025-04-03 09:45:14 +0800

    Update the code it should not be divided by zero

--- Program.cs
+++ Program.cs
@@ -2,5 +2,9 @@
 // this will divide the two numbers
 static double Divide(double a, double b)
 {
+    if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
     return a / b;
 }
