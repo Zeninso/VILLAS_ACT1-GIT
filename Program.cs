e09658664685c570b496ad6b9e40a812dd72da09
vincent-villas <vincentvillas0208@gmail.com>
2025-04-03 09:40:00 +0800

    heres the code for multiplication

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // this will subtract the two numbers
-static double Subtract(double a, double b)
+static double Multiplication(double a, double b)
 {
-    return a - b;
+    return a * b;
 }
