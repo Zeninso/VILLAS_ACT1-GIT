07c3b2234355e79ed121d9e3728a91bff0a7b638
Vincent Villas <152830911+Zeninso@users.noreply.github.com>
2025-04-03 10:03:03 +0800

    removing a duplicate comment

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// See https://aka.ms/new-console-template for more information
-// this will display the result of the operation you picked
+//This will display the result of the operation you picked
 static void DisplayResult(double num1, string operation, double num2, double result)
 {
     Console.WriteLine($"Result: {num1} {operation} {num2} = {result}");
